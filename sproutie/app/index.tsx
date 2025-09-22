3f96301b4322483d99f81440d182ee8856bb286d
Joseph Musngi <Josephmusngi7@gmail.com>
2025-09-22 13:00:39 -0700

    added flex to view

--- sproutie/app/index.tsx
+++ sproutie/app/index.tsx
@@ -1,18 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import Login from './login/index';
-import Register from './register/index';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Register />
-      
+    <View style={{ flex: 1 }}>
+      <Login />
     </View>
   );
 }
