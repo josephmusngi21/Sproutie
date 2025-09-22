ec06dc04d0da0c272c9aad1b46d311129edca73d
Joseph Musngi <Josephmusngi7@gmail.com>
2025-09-21 20:36:16 -0700

    created firebase config

---
+++ sproutie/app/firebase/config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+// import {...} from 'firebase/auth';
+// import {...} from 'firebase/database';
+// import {...} from 'firebase/firestore';
+// import {...} from 'firebase/functions';
+// import {...} from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
+
+const app = initializeApp(firebaseConfig);
