f0674c2a3a1a5df6994e294949e71d952f0c2d45
Joseph Musngi <Josephmusngi7@gmail.com>
2025-09-23 14:57:23 -0700

    added conditional render for Login and Register

--- sproutie/app/index.tsx
+++ sproutie/app/index.tsx
@@ -1,10 +1,15 @@
 import { View } from "react-native";
+import { useState } from "react";
 import Login from './login/index';
+import Register from './register/index';
 
 export default function Index() {
+  const [showLogin, setShowLogin] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+  
   return (
     <View style={{ flex: 1 }}>
-      <Login />
+      {showLogin ? <Login /> : <Register />}
     </View>
   );
 }
