26213a9c6bbc20c33fa1d445b2da219240b4f35d
Joseph Musngi <Josephmusngi7@gmail.com>
2025-09-23 15:22:17 -0700

    added new styling and conditional rendering for login and register and changes the footer too

--- sproutie/app/index.tsx
+++ sproutie/app/index.tsx
@@ -1,15 +1,33 @@
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useState } from "react";
 import Login from './login/index';
 import Register from './register/index';
+import styles from './styles'
 
 export default function Index() {
   const [showLogin, setShowLogin] = useState(true);
   const [loggedIn, setLoggedIn] = useState(false);
-  
+
+  const Footer = () => {
+    return (
+      <View style={styles.footer}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={styles.footerText}>
+            {showLogin ? "Don't have an account?" : "Already have an account?"}
+          </Text>
+          <TouchableOpacity onPress={() => setShowLogin(!showLogin)}>
+            <Text style={styles.footerLink}>
+              {showLogin ? " Register" : " Login"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       {showLogin ? <Login /> : <Register />}
+      <Footer />
     </View>
   );
 }
