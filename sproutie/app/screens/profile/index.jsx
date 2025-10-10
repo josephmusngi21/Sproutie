a57c7d63f84ee7274006ec9ccd701a7eb38a81e8
Joseph Musngi <Josephmusngi7@gmail.com>
2025-10-10 00:15:29 -0700

    initiated profileScreen

---
+++ sproutie/app/screens/profile/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import styles from './styles';
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Screen</Text>
+      <Text style={styles.subtitle}>Coming Soon...</Text>
+    </View>
+  );
+}
