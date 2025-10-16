c1cde5c00596e8a8758f6d67c1583aa4115b9744
Joseph Musngi <Josephmusngi7@gmail.com>
2025-10-16 16:09:54 -0700

    Add user data fetching logic to ProfileScreen
    Introduced a fetchUserData async function using apiService to retrieve user info based on UID. Added loading state management and logging for API calls. The UI remains unchanged for now.

--- sproutie/app/screens/profile/index.jsx
+++ sproutie/app/screens/profile/index.jsx
@@ -1,11 +1,29 @@
 import { View, Text } from "react-native";
+import { useState } from "react";
 import styles from './styles';
+import apiService from "../../services/apiService";
 
-export default function ProfileScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile Screen</Text>
-      <Text style={styles.subtitle}>Coming Soon...</Text>
-    </View>
-  );
+export default function ProfileScreen(UID) {
+    const [loading, setLoading] = useState(false);
+    const userInfo = null;
+
+    const fetchUserData = async () => {
+        try {
+            userInfo = await apiService.getUser(UID);
+            setLoading(true)
+            console.log("User info fetched from API:", userInfo);
+        } catch {
+            console.error("Error fetching user info from API");
+        } finally {
+            setLoading(false);
+            console.log("Loading done");
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Profile Screen</Text>
+            <Text style={styles.subtitle}>Coming Soon...</Text>
+        </View>
+    )
 }
